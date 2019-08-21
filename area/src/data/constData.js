bb4f25dfa98ca10cc4a77f1fc39c1d46971c45ca
cometang <1403029829@qq.com>
2019-08-21 20:33:24 +0800

    正在处理 贵州省单证书打印

--- area/src/data/constData.js
+++ area/src/data/constData.js
@@ -33,7 +33,7 @@ const examineStatus = [{key:0,val:'等待组织审核'},{key:1,val:'组织审核
 /**
  * 打印的股权证的页面分类
  * */
-const printType = [{key:0,val:'成员股权证'},{key:1,val:'初始登记'},{key:2,val:'变更登记'},{key:3,val:'抵押情况登记'}]
+const printType = [{key:0,val:'成员股权证'},{key:1,val:'初始登记'},{key:2,val:'变更登记'},{key:3,val:'抵押情况登记'},{key:4,val:'贵州省股东基本信息登记'},{key:5,val:'贵州省股权登记'},{key:6,val:'贵州省股权变更登记'},{key:7,val:'贵州省收益分配领取记录'}]
 
 /**
  * 选择打印变量的前端定义变量
@@ -41,9 +41,78 @@ const printType = [{key:0,val:'成员股权证'},{key:1,val:'初始登记'},{key
 const printConstant =[{key:1,isConstant:1,printingName:'当天日期',printing:''}]
 
 /** 导入表格常量**/
+
 const importData = {userImport:{skipRowCount:1,colCount:17},assetImport:{skipRowCount:2,colCount:38}}
 
-// const
+/**打印证书模板 图片列表 */
+
+const printBookList = {
+    huichuan:[
+        {
+            id:'hc1',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/1.jpg',
+            seat:'右侧',
+            info:'成员股权证首页'
+        },
+        {
+            id:'hc1',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/2.jpg',
+            seat:'右侧',
+            info:'初始登记页'
+        },
+        {
+            id:'hc1',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/3.jpg',
+            seat:'左侧',
+            info:'变更登记页1'
+        },
+        {
+            id:'hc1',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/3.jpg',
+            seat:'右侧',
+            info:'变更登记页2'
+        },
+        {
+            id:'hc1',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/4.jpg',
+            seat:'左侧',
+            info:'抵押情况登记页'
+        }
+    ],
+    guizhou:[
+        {
+            id:'s1',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/s1.png',
+            seat:'右侧',
+            info:'股东基本信息登记页'
+        },
+        {
+            id:'s2',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/s2.png',
+            seat:'左侧',
+            info:'股权登记页'
+        },
+        {
+            id:'s3',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/s3.png',
+            seat:'右侧',
+            info:'变更登记页'
+        },
+        {
+            id:'s4',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/s4.png',
+            seat:'左侧',
+            info:'收益分配领取记录页'
+        },
+        {
+            id:'s5',
+            src:'http://jitijingji-test1.oss-cn-hangzhou.aliyuncs.com/template/s4.png',
+            seat:'右侧',
+            info:'收益分配领取记录页'
+        }
+    ]
+}
+
 
 export default {
     grade,
@@ -53,5 +122,6 @@ export default {
     examineStatus,
     printConstant,
     printType,
-    importData
+    importData,
+    printBookList
 }
---
+++ area/src/views/area/print/printTemplateView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="checkIndex">
+        <template v-if="checkIndex == 1">
+            <areaPrintViewInfo></areaPrintViewInfo>
+        </template>
+        <template v-else-if="checkIndex ==2">
+            <areaPrintViewInfo1></areaPrintViewInfo1>
+        </template>
+        <template v-else-if="checkIndex ==3">
+            <areaPrintViewInfo2></areaPrintViewInfo2>
+        </template>
+        <template v-else-if="checkIndex ==4">
+            <areaPrintViewInfo3></areaPrintViewInfo3>
+        </template>
+        <template v-else>
+            <areaPrintViewInfo4></areaPrintViewInfo4>
+        </template>
+    </div>
+</template>
+
+<script>
+    import areaPrintViewInfo from './component/huichuan/templateView/printViewInfo'
+    import areaPrintViewInfo1 from './component/huichuan/templateView/printViewInfo1'
+    import areaPrintViewInfo2 from './component/huichuan/templateView/printViewInfo2'
+    import areaPrintViewInfo3 from './component/huichuan/templateView/printViewInfo3'
+    import areaPrintViewInfo4 from './component/huichuan/templateView/printViewInfo4'
+    export default {
+        name: "printTemplateView",
+        data(){
+            return{
+                checkIndex:'',
+            }
+        },
+        components:{
+            areaPrintViewInfo,
+            areaPrintViewInfo1,
+            areaPrintViewInfo2,
+            areaPrintViewInfo3,
+            areaPrintViewInfo4
+        },
+        mounted(){
+            this.checkIndex = this.$route.params.id
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
