3a186e988c7ed6018d354c72d42bc98eab2376b4
cometang <1403029829@qq.com>
2019-10-31 20:43:15 +0800

    no message

--- pc/babel.config.js
+++ pc/babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  sourceType: 'unambiguous'  //exprots 转码
+
 }
--- pc/vue.config.js
+++ pc/vue.config.js
@@ -1,5 +1,6 @@
-export default{
+
+module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
-    ? './'
-    : './'
+      ? './'
+      : './'
 }
